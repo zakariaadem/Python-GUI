59201e92c4e588431028d31032f401e981254935
zakariaadem <zakaria1adam1@gmail.com>
2022-02-02 15:14:39 +0100

    added tabs

--- py gui.py
+++ py gui.py
@@ -2,20 +2,17 @@ import PySimpleGUI as sg
  
 # Design pattern 2 - First window remains active
 
+
 def win1_layout():
     layout = [
-        [sg.Text('Registrering av varer', text_color='black')],
-        [sg.Text('Varenummer', size =(15, 1)), sg.InputText(key='Varenummer')],
-        [sg.Text('Produktnavn', size =(15, 1)), sg.InputText(key='Produktnavn')],
-        [sg.Text('Beskrivelse', size =(15, 1)), sg.InputText(key='Beskrivelse')],
-        [sg.Text('Pris', size =(15, 1)), sg.InputText(key='Pris')],
-        [sg.Text('Kategori', size =(15, 1)), sg.InputText(key='Kategori')],
-        [sg.Text('Lager', size =(15, 1)), sg.InputText(key='Lager')],
-        [sg.Text('Bilde src', size =(15, 1)), sg.InputText(key='Bilde src')],
-        [sg.Button('Lagre')],
+        [sg.Text('Brukernavn', size=(15, 1)), sg.InputText(key='brukernavn', size=(30, 10))],
+        [sg.Text('Passord', size=(15, 1)), sg.InputText(key='passord', size=(30, 10))],
+        [sg.Button('Logg inn', key='logginn')],
     ]
- 
+
     return layout
+
+
 
 def win2_layout():
     layout = [
@@ -34,33 +31,51 @@ def win2_layout():
 
 def win3_layout():
     layout = [
-        [sg.Text('Brukernavn', size=(15, 1)), sg.InputText(key='brukernavn', size=(30, 10))],
-        [sg.Text('Passord', size=(15, 1)), sg.InputText(key='passord', size=(30, 10))],
-        [sg.Button('Logg inn', key='logginn')],
+        [sg.Text('Registrering av varer', text_color='black')],
+        [sg.Text('Varenummer', size=(15, 1)), sg.InputText(key='Varenummer')],
+        [sg.Text('Produktnavn', size=(15, 1)), sg.InputText(key='Produktnavn')],
+        [sg.Text('Beskrivelse', size=(15, 1)), sg.InputText(key='Beskrivelse')],
+        [sg.Text('Pris', size=(15, 1)), sg.InputText(key='Pris')],
+        [sg.Text('Kategori', size=(15, 1)), sg.InputText(key='Kategori')],
+        [sg.Text('Lager', size=(15, 1)), sg.InputText(key='Lager')],
+        [sg.Text('Bilde src', size=(15, 1)), sg.InputText(key='Bilde src')],
+        [sg.Button('Lagre', key='lagre')],
     ]
 
     return layout
 
 
-win1 = sg.Window('Registrering', win1_layout())
+
+def win4_layout():
+    layout = [
+        [sg.Text('Finn vare', text_color='black')],
+        [sg.Text('Varenummer', size =(15, 1)), sg.InputText(key='Varenummer_finn')],
+        [sg.Button('Finn', key='finn')],
+    ]
+    return layout
+
+tabgrp = [[sg.TabGroup([[sg.Tab('Finn vare', win4_layout()),
+                         sg.Tab('Registrering av vare', win3_layout())]])]]
+
+win1 = sg.Window('logg inn', win1_layout(), return_keyboard_events= True, finalize= True)
 win2 = sg.Window('Registrering', win2_layout())
-win3 = sg.Window('logg inn', win3_layout(), return_keyboard_events= True, finalize= True)
-win3['passord'].bind("<Return>", "_Enter")
+win3 = sg.Window('Registrering', win3_layout())
+win1['passord'].bind("<Return>", "_Enter")
+win4 = sg.Window('Finn', win4_layout())
+tab_win = sg.Window("tabs", tabgrp)
 
 
 while True:
     ev1, vals1 = win3.read(timeout=100)
-    if ev1 == sg.WIN_CLOSED or ev1 == 'Escape:27':
+    ev2, vals2 = win1.read(timeout=100)
+    if ev2 == sg.WIN_CLOSED or ev2 == 'Escape:27':
         break
-    if not vals1['brukernavn'] == '' and not vals1['passord'] == '':
-        if ev1 == 'passord' + '_Enter':
-            win1.read()
-    if ev1 is not None:
-        print(ev1)
+    if not vals2['brukernavn'] == '' and not vals2['passord'] == '':
+        if ev2 == 'passord' + '_Enter' or ev2 == 'logginn':
+            win1.Hide()
+            tab_win.read()
 
-
-    if ev1 == 'Lagre':
-        win1.Hide()
+    if ev1 == 'lagre':
         win2.read(timeout=100)
         win2['Varenummer2'].update(vals1['Varenummer'])
         win2['Produktnavn2'].update(vals1['Produktnavn'])
@@ -69,6 +84,8 @@ while True:
         win2['Kategori2'].update(vals1['Kategori'])
         win2['Lager2'].update(vals1['Lager'])
         win2['Bilde src2'].update(vals1['Bilde src'])
+
+
 
 
 
